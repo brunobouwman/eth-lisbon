52b129e1f751398753bec2f2d930a2bf1ab48f84
Guilherme Brandão <guilherme1237@gmail.com>
2023-11-04 13:58:29 +0000

    Added google endpoints

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			authorization: {
+				url: "https://accounts.google.com/o/oauth2/v2/auth",
+				params: {
+					prompt: "consent",
+					access_type: "offline",
+					response_type: "code",
+					scope: "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.blood_glucose.read https://www.googleapis.com/auth/fitness.blood_pressure.read https://www.googleapis.com/auth/fitness.body.read https://www.googleapis.com/auth/fitness.body_temperature.read https://www.googleapis.com/auth/fitness.heart_rate.read https://www.googleapis.com/auth/fitness.location.read https://www.googleapis.com/auth/fitness.nutrition.read https://www.googleapis.com/auth/fitness.oxygen_saturation.read https://www.googleapis.com/auth/fitness.reproductive_health.read https://www.googleapis.com/auth/fitness.sleep.read",
+				},
+			},
+		}),
+	],
+	jwt: {
+		encryption: true,
+	},
+	secret: process.env.SECRET,
+	callbacks: {
+		async jwt({ token, user, account }) {
+			if (account && user) {
+				token.refreshToken = account.refresh_token;
+			}
+			console.log("token", JSON.stringify(token));
+			return token;
+		},
+		async session({ session, token }) {
+			if (token) {
+				session.refreshToken = token.refreshToken;
+			}
+			console.log("token", JSON.stringify(token));
+			console.log("session", JSON.stringify(session));
+			return session;
+		},
+	},
+});
